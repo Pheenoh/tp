dd2d3f4d884db85b9731cae342c0d7f2f2d01988
Pheenoh <Pheenoh@users.noreply.github.com>
2023-02-20 05:32:25 +0000

    deploy: 8471d1d6ccb714dbf5968b2ad9da8b1be6b4919e

--- html/search/all_b.js
+++ html/search/all_b.js
@@ -38,7 +38,7 @@ var searchData=
   ['ketteitxtdispanm_35',['ketteiTxtDispAnm',['../structdMenu__save__c.html#a71be821a5360762fffc1862aabf8bc91',1,'dMenu_save_c::ketteiTxtDispAnm()'],['../structdFile__select__c.html#a21cbe726827fd6ca380c8cbd4f73e2b9',1,'dFile_select_c::ketteiTxtDispAnm()']]],
   ['ketteitxtdispanm_5f_5f12dmenu_5fsave_5fcfv_36',['ketteiTxtDispAnm__12dMenu_save_cFv',['../d__menu__save_8cpp.html#a2d71cff61b2e97a6ce7fbaa35a0355f7',1,'d_menu_save.cpp']]],
   ['ketteitxtdispanm_5f_5f14dfile_5fselect_5fcfv_37',['ketteiTxtDispAnm__14dFile_select_cFv',['../d__file__select_8cpp.html#aa0e6ff4b255e4d331a01d50f604743aa',1,'d_file_select.cpp']]],
-  ['ketteitxtdispanminit_38',['ketteiTxtDispAnmInit',['../structdMenu__save__c.html#a244cf321ace2206b9b9b83a46bc90bf2',1,'dMenu_save_c::ketteiTxtDispAnmInit()'],['../structdFile__select__c.html#abf193eb5f176769bb25a879a2099950b',1,'dFile_select_c::ketteiTxtDispAnmInit()']]],
+  ['ketteitxtdispanminit_38',['ketteiTxtDispAnmInit',['../structdFile__select__c.html#abf193eb5f176769bb25a879a2099950b',1,'dFile_select_c::ketteiTxtDispAnmInit()'],['../structdMenu__save__c.html#a244cf321ace2206b9b9b83a46bc90bf2',1,'dMenu_save_c::ketteiTxtDispAnmInit()']]],
   ['ketteitxtdispanminit_5f_5f12dmenu_5fsave_5fcfuc_39',['ketteiTxtDispAnmInit__12dMenu_save_cFUc',['../d__menu__save_8cpp.html#a9e2bec5ea3fdd04fbe9faecf23944000',1,'d_menu_save.cpp']]],
   ['ketteitxtdispanminit_5f_5f14dfile_5fselect_5fcfuc_40',['ketteiTxtDispAnmInit__14dFile_select_cFUc',['../d__file__select_8cpp.html#ae54fc179431d7bdcb1885f720ae4767b',1,'d_file_select.cpp']]],
   ['key_5fof_5fcaravan_41',['KEY_OF_CARAVAN',['../d__save_8h.html#a0a48966e59817e2d4685d1f0b44b33f2aed18bef730e62a7292506619a0ce34ba',1,'d_save.h']]],
@@ -54,7 +54,7 @@ var searchData=
   ['kill_51',['Kill',['../structdVibration__c.html#a6748285980082eddcf9e7e711b107e35',1,'dVibration_c']]],
   ['kill_5f_5f12dvibration_5fcfv_52',['Kill__12dVibration_cFv',['../d__vibration_8cpp.html#a7d26492db7b443a4fb87d1f984486655',1,'d_vibration.cpp']]],
   ['killactivechannel_53',['killActiveChannel',['../structJASDSPChannel.html#ae37a21ea6116e6d5b766b7c8463565d1',1,'JASDSPChannel::killActiveChannel()'],['../structJASDSPChannel.html#a87dcbdd21e0601e4ebf0a93d3c018c4e',1,'JASDSPChannel::killActiveChannel()']]],
-  ['killactivechannel_5f_5f13jasdspchannelfv_54',['killActiveChannel__13JASDSPChannelFv',['../JASAiCtrl_8cpp.html#aec6fb04a5f93b5fde9a125aba8277c19',1,'killActiveChannel__13JASDSPChannelFv():&#160;JASAiCtrl.cpp'],['../JASDSPChannel_8cpp.html#aec6fb04a5f93b5fde9a125aba8277c19',1,'killActiveChannel__13JASDSPChannelFv():&#160;JASDSPChannel.cpp']]],
+  ['killactivechannel_5f_5f13jasdspchannelfv_54',['killActiveChannel__13JASDSPChannelFv',['../JASDSPChannel_8cpp.html#aec6fb04a5f93b5fde9a125aba8277c19',1,'killActiveChannel__13JASDSPChannelFv():&#160;JASDSPChannel.cpp'],['../JASAiCtrl_8cpp.html#aec6fb04a5f93b5fde9a125aba8277c19',1,'killActiveChannel__13JASDSPChannelFv():&#160;JASAiCtrl.cpp']]],
   ['killsubcontents_55',['killSubContents',['../classdMeter2__c.html#a2e26c996ab1ab5efd0bbf57ab6d366d8',1,'dMeter2_c']]],
   ['killsubcontents_5f_5f9dmeter2_5fcfuc_56',['killSubContents__9dMeter2_cFUc',['../d__meter2_8cpp.html#aa4b5a9a0afed63f5ef1d5b58821b9685',1,'d_meter2.cpp']]],
   ['killthreads_57',['KillThreads',['../OSReset_8h.html#afd6e0ba5a2f96cd39ff3e4146be34d44',1,'KillThreads(void):&#160;OSReset.h'],['../OSReset_8c.html#afd6e0ba5a2f96cd39ff3e4146be34d44',1,'KillThreads(void):&#160;OSReset.c']]],
@@ -70,6 +70,7 @@ var searchData=
   ['kokiri_5fclothes_5fflag_67',['KOKIRI_CLOTHES_FLAG',['../d__save_8h.html#a754ce34cf5161a6dc781544dbecc6cd0aa8e82eaa10422a66792fb9c246061c4a',1,'d_save.h']]],
   ['kvcdvatdlsize_68',['kVcdVatDLSize',['../J3DShape_8cpp.html#adf764cbdea00d65edcd07bb9953ad2b7a1587d173f2bd09d94ac8cf4004fb6bc2',1,'kVcdVatDLSize():&#160;J3DShape.cpp'],['../classJ3DShape.html#a3401d7061c64290601a663b0d12fd9daa78df67ba3112e67a6e32f56478e5f788',1,'J3DShape::kVcdVatDLSize()']]],
   ['kytag05_5fclass_69',['kytag05_class',['../classkytag05__class.html',1,'']]],
-  ['kytag14_5fclass_70',['kytag14_class',['../classkytag14__class.html',1,'']]],
-  ['kytag17_5fclass_71',['kytag17_class',['../classkytag17__class.html',1,'']]]
+  ['kytag07_5fclass_70',['kytag07_class',['../classkytag07__class.html',1,'']]],
+  ['kytag14_5fclass_71',['kytag14_class',['../classkytag14__class.html',1,'']]],
+  ['kytag17_5fclass_72',['kytag17_class',['../classkytag17__class.html',1,'']]]
 ];
--- html/search/namespaces_1.js
+++ html/search/namespaces_1.js
@@ -1,8 +1,9 @@
 var searchData=
 [
-  ['daitem_5fprm_0',['daItem_prm',['../namespacedaItem__prm.html',1,'']]],
-  ['daobj_1',['daObj',['../namespacedaObj.html',1,'']]],
-  ['datagrestart_5fprm_2',['daTagRestart_prm',['../namespacedaTagRestart__prm.html',1,'']]],
-  ['datagspinner_5fprm_3',['daTagSpinner_prm',['../namespacedaTagSpinner__prm.html',1,'']]],
-  ['datboxsw_5fprm_4',['daTboxSw_prm',['../namespacedaTboxSw__prm.html',1,'']]]
+  ['daattp_5fprm_0',['daAttp_prm',['../namespacedaAttp__prm.html',1,'']]],
+  ['daitem_5fprm_1',['daItem_prm',['../namespacedaItem__prm.html',1,'']]],
+  ['daobj_2',['daObj',['../namespacedaObj.html',1,'']]],
+  ['datagrestart_5fprm_3',['daTagRestart_prm',['../namespacedaTagRestart__prm.html',1,'']]],
+  ['datagspinner_5fprm_4',['daTagSpinner_prm',['../namespacedaTagSpinner__prm.html',1,'']]],
+  ['datboxsw_5fprm_5',['daTboxSw_prm',['../namespacedaTboxSw__prm.html',1,'']]]
 ];
