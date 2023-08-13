297622068419ff810e50552c6fa016f385fe4508
Pheenoh <Pheenoh@users.noreply.github.com>
2023-08-13 10:13:27 +0000

    deploy: 08fa2f03fc1333057e265e80dd7ba8b1fc59b472

--- html/search/namespaces_1.js
+++ html/search/namespaces_1.js
@@ -1,11 +1,12 @@
 var searchData=
 [
   ['daattp_5fprm_0',['daAttp_prm',['../namespacedaAttp__prm.html',1,'']]],
-  ['daitem_5fprm_1',['daItem_prm',['../namespacedaItem__prm.html',1,'']]],
-  ['daobj_2',['daObj',['../namespacedaObj.html',1,'']]],
-  ['daobjeff_3',['daObjEff',['../namespacedaObjEff.html',1,'']]],
-  ['datagmist_5fprm_4',['daTagMist_prm',['../namespacedaTagMist__prm.html',1,'']]],
-  ['datagrestart_5fprm_5',['daTagRestart_prm',['../namespacedaTagRestart__prm.html',1,'']]],
-  ['datagspinner_5fprm_6',['daTagSpinner_prm',['../namespacedaTagSpinner__prm.html',1,'']]],
-  ['datboxsw_5fprm_7',['daTboxSw_prm',['../namespacedaTboxSw__prm.html',1,'']]]
+  ['dabgobj_5fprm_1',['daBgObj_prm',['../namespacedaBgObj__prm.html',1,'']]],
+  ['daitem_5fprm_2',['daItem_prm',['../namespacedaItem__prm.html',1,'']]],
+  ['daobj_3',['daObj',['../namespacedaObj.html',1,'']]],
+  ['daobjeff_4',['daObjEff',['../namespacedaObjEff.html',1,'']]],
+  ['datagmist_5fprm_5',['daTagMist_prm',['../namespacedaTagMist__prm.html',1,'']]],
+  ['datagrestart_5fprm_6',['daTagRestart_prm',['../namespacedaTagRestart__prm.html',1,'']]],
+  ['datagspinner_5fprm_7',['daTagSpinner_prm',['../namespacedaTagSpinner__prm.html',1,'']]],
+  ['datboxsw_5fprm_8',['daTboxSw_prm',['../namespacedaTboxSw__prm.html',1,'']]]
 ];
